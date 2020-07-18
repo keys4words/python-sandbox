0ca52c23431e17d43531d8458e17b6ef3cb0dcca
keys4words <keys4words@gmail.com>
2020-07-18 21:57:45 +0300

    add sequence intro

--- itvdn-essential/sequences/my_getitem.py
+++ itvdn-essential/sequences/my_getitem.py
@@ -1,2 +1,9 @@
-values = [1, 3, 5]
-print(values.__getitem__(1), values[1])
+values = [1, 3, 5, 4, 22, 911]
+# print(values.__getitem__(1), values[1])
+# print(values.__getitem__(slice(2,5)))
+print(values)
+del values[:2]
+values.__delitem__(-1)
+print(values)
+# import collections.abc
+# print(dir(collections.abc.MutableSequence))
