ef34ba7172c5d4668224f55196515ef1e75ba58b
keys4words <keys4words@gmail.com>
2020-07-19 16:09:11 +0300

    Add merging func to algo

---
+++ ch_algo/merging_sort.py
@@ -0,0 +1,21 @@
+def merging(arr1, arr2):
+    sorted_arr = []
+    i, j = 0, 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            sorted_arr.append(arr1[i])
+            i += 1
+        else:
+            sorted_arr.append(arr2[j])
+            j += 1
+    while i < len(arr1):
+        sorted_arr.append(arr1[i])
+        i += 1
+    while j < len(arr2):
+        sorted_arr.append(arr2[j])
+        j += 1
+    return sorted_arr
+
+l1 = [1, 4, 6, 8, 10]
+l2 = [2, 3, 5, 7, 8, 9]
+print(f"merging list: {merging(l1, l2)}")
--- sockets/main.py
+++ sockets/main.py
@@ -1,7 +1,38 @@
 import socket
 
+URLS = {
+    '/': 'that is index',
+    '/blog': 'it is blog'
+}
+
+def generate_content(code, url):
+    if code == 404:
+        return '<h1>404</h1><p>Not found</p>'
+    if code == 405:
+        return '<h1>405</h1><p>Method not allowed</p>'
+    return '<h1>{}</h1>'.format(URLS[url])
+    
+
+def parse_request(request):
+    parsed = request.split(' ')
+    method = parsed[0]
+    url = parsed[1]
+    return (method, url)
+
+def generate_headers(method, url):
+    if not method == 'GET':
+        return ('HTTP/1.1 405 Method not allowed\n\n', 405)
+    if not url in URLS:
+        return ('HTTP/1.1 404 Not found\n\n', 404)
+
+    return ('HTTP/1.1 200 OK\n\n', 200)
+
 def generate_response(request):
-    pass
+    method, url = parse_request(request)
+    headers, code = generate_headers(method, url)
+    body = generate_content(code, url)
+
+    return (headers + body).encode()
 
 def run():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +49,7 @@ def run():
 
         response = generate_response(request.decode('utf-8'))
 
-        client_socket.sendall('hello world'.encode())
+        client_socket.sendall(response)
         client_socket.close()
 
 
