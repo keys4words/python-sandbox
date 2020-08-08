dbdb97a65c11b5253116478134891e9c83dec392
keys4words <keys4words@gmail.com>
2020-08-08 15:27:03 +0300

    add simple ioexample

---
+++ inout/ioexample.py
@@ -0,0 +1,23 @@
+import os.path
+
+with open(os.path.join('inout', 'data', 'example.txt')) as myfile:
+    print(myfile.read())
+# myfile.close()
+
+
+class MyManager:
+    def __init__(self):
+        self.resource = 43
+
+    def __enter__(self):
+        print('Entered context')
+        return self.resource
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        print('Left context')
+        if exc_type:
+            print('Exception occured: {}'.format(exc_type))
+
+with MyManager() as resource:
+    print("some actions with resource:", resource)
+    raise ValueError
