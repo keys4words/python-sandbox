9e20a169edc63b46d4a9667247b78ecbc317d58d
keys4words <keys4words@gmail.com>
2020-08-26 13:42:26 +0300

    add http module

---
+++ http/http_via_urllib.py
@@ -0,0 +1,11 @@
+from urllib import request
+
+response = request.urlopen('http://example.com')
+print(response.status)
+print(response.getcode())
+print(response.msg)
+print(response.reason)
+print(response.headers)
+print(response.getheaders())
+print(response.headers.get('Content-Type'))
+print(response.getheader('Content-Type'))
