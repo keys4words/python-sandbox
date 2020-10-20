b0c7140ce53bfd98bdc3e869daded092f5fb133f
keys4words <keys4words@gmail.com>
2020-10-20 21:06:36 +0300

    add json formatting log

---
+++ loggingPractice/les03.py
@@ -0,0 +1,30 @@
+import logging.config
+
+from settings import logger_config
+
+
+logging.config.dictConfig(logger_config)
+
+logger = logging.getLogger('app_logger')
+
+
+# logger.setLevel('DEBUG')
+# console_handler = logging.StreamHandler()
+# logger.addHandler(console_handler)
+# print(console_handler.level)
+
+# std_format = logging.Formatter(fmt='{asctime} - {levelname} - {name} - {message}', style='{')
+# console_handler.setFormatter(std_format)
+
+# file_handler = logging.FileHandler('debug.log')
+# logger.addHandler(file_handler)
+# file_handler.setFormatter(std_format)
+
+
+def main():
+    logger.debug('In the main()')
+
+
+
+if __name__ == "__main__":
+    main()
